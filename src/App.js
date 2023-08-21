119f80700de18099ce6c06b267d800f135bc2df3
AnaLuisGuerreiro <124161000+AnaLuisGuerreiro@users.noreply.github.com>
2023-08-21 23:13:31 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import "./App.css";
+import Weather from "Weather";
 
 function App() {
   return (
     <div className="App">
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://portfolio-anaguerreiro.netlify.app/"
-          alt="Ana Guerreiro"
-        >
-          {" "}
-          Ana Guerreiro
-        </a>{" "}
-        and is{" "}
-        <a href="" alt="Github source">
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://portfolio-anaguerreiro.netlify.app/"
+            alt="Ana Guerreiro"
+          >
+            {" "}
+            Ana Guerreiro
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/AnaLuisGuerreiro/weather-app-react"
+            alt="Github source"
+          >
+            open-sourced on Github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
